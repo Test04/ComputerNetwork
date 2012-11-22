223985181a4bef9f72616b257eab38c31fed5954
zhengyu <zhengyu_sysu@foxmail.com>
2012-11-22 17:02:04 +0800

    a little change

--- hello.java
+++ hello.java
@@ -3,7 +3,10 @@ public class hello {
 	public static void main(String[] args)
 	{
 		sayhello();
+
 	}
+	
+	// a little change
 	public static void sayhello()
 	{
 		System.out.println("hello world");
